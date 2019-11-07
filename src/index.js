93c5fbb4537a077ef5b06d3a18c4cb31ac89cb70
jackie-gan <jackiegan405679096@gmail.com>
2019-11-07 23:26:20 +0800

    feat: replade string

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { createFilter } from 'rollup-pluginutils';
 
-export default function tinyTransfrom(options) {
+export default function tinyTransfrom(options = {}) {
   const filter = createFilter(options.include, options.exclude);
 
   return {
     name: 'tiny-transform',
     transform (code, id) {
       if (!filter(id)) return;
-      console.log(111, code);
-      return {
-        code: code,
-        
-      }
+
+      const reg = /\@tarojs\/taro/;
+      const newCode = code.replace(reg, '@tarojs/taro-h5');
+
+      return newCode
     }
   }
 }
