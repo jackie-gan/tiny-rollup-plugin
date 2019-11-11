1a1209524d0d547553354450a130eb2be54c3b10
ganzhanjian <ganzhanjian@mucfc.com>
2019-11-11 11:50:55 +0800

    feat: add babel core

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import { createFilter } from 'rollup-pluginutils';
+import { transform } from 'babel-core';
 
 export default function tinyTransfrom(options = {}) {
   const filter = createFilter(options.include, options.exclude);
+  const replaceString = options.replaceString || '';
+
+  const codeVistor = {
+    ImportDeclaration(path) {
+      const importValue = path.node.source.value;
+      const reg = /\@tarojs\/taro/;
+      const newCode = importValue.replace(reg, '@tarojs/taro-h5');
+      path.node.source.value = newCode;
+    }
+  };
 
   return {
     name: 'tiny-transform',
     transform (code, id) {
       if (!filter(id)) return;
 
-      const reg = /\@tarojs\/taro/;
-      const newCode = code.replace(reg, '@tarojs/taro-h5');
+      const transformRes = transform(code, {
+        plugins: {
+          visitor: codeVistor
+        }
+      });
 
-      return newCode
+      console.log(transformRes.code);
+
+      return transformRes.code
     }
   }
 }
